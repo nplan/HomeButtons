86d09988b8e3188db59980a1d6c04a18d06b41cc
Nejc Planinšek <planinseknejc@gmail.com>
2023-02-05 13:45:53 +0100

    version bump

--- Firmware/HomeButtonsArduino/src/config.h
+++ Firmware/HomeButtonsArduino/src/config.h
@@ -5,7 +5,7 @@
 
 // ------ device ------
 static constexpr char MANUFACTURER[] = "PLab";
-static constexpr char SW_VERSION[] = "v2.0.3-beta";
+static constexpr char SW_VERSION[] = "v2.0.3";
 static constexpr char DOCS_LINK[] = "https://docs.home-buttons.com/setup";
 
 // ------ wifi AP ------
