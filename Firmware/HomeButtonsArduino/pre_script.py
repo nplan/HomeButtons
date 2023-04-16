01eb4a19d779e49435162b29d1b392129b214a93
Nejc Planinsek <planinseknejc@gmail.com>
2023-04-16 18:47:58 +0200

    added script that deletes sdkconfig.release & sdkconfig.debug berfore build

---
+++ Firmware/HomeButtonsArduino/pre_script.py
@@ -0,0 +1,13 @@
+import os
+
+
+def delete_sdkconfig_files():
+    try:
+        os.remove("sdkconfig.release")
+        os.remove("sdkconfig.debug")
+        print("Deleted sdkconfig.release and sdkconfig.debug files")
+    except:
+        pass
+
+
+delete_sdkconfig_files()
