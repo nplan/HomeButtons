16708d277d8add0f776e35995e14883a0c695bee
Nejc Planinsek <planinseknejc@gmail.com>
2023-05-09 21:14:34 +0200

    added post script to generate firmware.zip

--- Firmware/HomeButtonsArduino/pre_script.py
+++ Firmware/HomeButtonsArduino/pre_script.py
@@ -2,14 +2,21 @@
 
 import os
 
+sdkconfig_files = ["sdkconfig.release", "sdkconfig.debug"]
 
 def delete_sdkconfig_files():
-    try:
-        os.remove("sdkconfig.release")
-        os.remove("sdkconfig.debug")
-        print("Deleted sdkconfig.release and sdkconfig.debug files")
-    except:
-        pass
+    print("Deleting sdkconfig files...")
+
+    for file in sdkconfig_files:
+        if os.path.isfile(file):
+            try:
+                os.remove(file)
+            except:
+                print("Failed to delete {}".format(file))
+            else:
+                print("Deleted {}".format(file))
 
 
+print("#### PRE SCRIPT ####")
 delete_sdkconfig_files()
+print("#### PRE SCRIPT DONE ####")
