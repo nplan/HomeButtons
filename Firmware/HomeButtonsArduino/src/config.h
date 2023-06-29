0df8b46f18e3fbb4eee812bec5bfc1ef1e518fac
Nejc Planinsek <planinseknejc@gmail.com>
2023-06-29 17:39:19 +0200

    prepend '_' to force small font

--- Firmware/HomeButtonsArduino/src/config.h
+++ Firmware/HomeButtonsArduino/src/config.h
@@ -6,7 +6,7 @@
 
 // ------ device ------
 static constexpr char MANUFACTURER[] = "PLab";
-static constexpr char SW_VERSION[] = "v2.2.1-beta";
+static constexpr char SW_VERSION[] = "v2.2.1-beta2";
 
 // ------ URLs ------
 #ifndef HOMEBUTTONS_MINI
