f685eb919a4b91e4cc8b9240d5b735a71cc95a38
Nejc Planinsek <planinseknejc@gmail.com>
2023-07-11 20:07:08 +0200

    version bump v2.3.0

--- Firmware/HomeButtonsArduino/src/config.h
+++ Firmware/HomeButtonsArduino/src/config.h
@@ -6,7 +6,7 @@
 
 // ------ device ------
 static constexpr char MANUFACTURER[] = "PLab";
-static constexpr char SW_VERSION[] = "v2.2.2-beta2";
+static constexpr char SW_VERSION[] = "v2.3.0";
 
 // ------ URLs ------
 #ifndef HOMEBUTTONS_MINI
